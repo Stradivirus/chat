a7c803aeddba45680ed07b4cf15848b24fee726f
stradivirus <stradivirus9@gmail.com>
2024-11-15 03:05:03 +0900

    젠킨스 테스트

--- front/src/App.js
+++ front/src/App.js
@@ -137,7 +137,7 @@ function App() {
     <div className="App">
       <div className="main-section">
         <header className="main-header">
-          <h1>채팅 애플리케이션</h1>
+          <h1>다소니 - 채팅</h1>
         </header>
         <main className="main-content">
           {showInfo ? (
