bb5a41d16237591aff34da428dbd6a7b4d88838d
stradivirus <stradivirus9@gmail.com>
2025-05-15 09:38:55 +0900

    오류 수정2

--- front/src/urls.js
+++ front/src/urls.js
@@ -1,7 +1,6 @@
-// src/urls.js
-// 로컬로 바로 실행할 경우
-const BASE_URL = 'http://web:8000';
-//const BASE_URL = 'http://localhost:8000';
+// 개발 환경: 로컬에서 실행 시 localhost 사용
+// 프로덕션: Docker 내부에서 web 서비스명 사용
+const BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:8000';
 
 export const URLS = {
   API_BASE_URL: BASE_URL,
